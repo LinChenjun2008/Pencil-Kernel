a198de77287836663304da20cd34df79b3ecd0d3
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-16 05:52:15 -0600

    Create imgcopy.h

---
+++ tools/imgcopy/imgcopy.h
@@ -0,0 +1,23 @@
+#ifndef __IMGCOPY_H_
+#define __IMGCOPY_H_
+
+#include "stdint.h"
+
+#ifndef _MAX_NAME_LEN_
+    #define MAX_NAME_LEN 15
+#endif
+
+struct file_desc              //64B
+{
+    char name[MAX_NAME_LEN];//15B
+    uint8_t f_type;           // 1B
+    uint32_t f_size;          // 4B
+    uint32_t sectors[11];     //44B
+};
+
+struct index_block
+{
+    uint32_t sectors[128];    //4*128=512B
+};
+
+#endif /* __IMGCOPY_H_ */
