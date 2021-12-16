0da372f289009a4b2e95a87bccfd72ef6b4969ce
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-16 05:35:39 -0600

    Create config.h

---
+++ src/arch/x86/include/config.h
@@ -0,0 +1,13 @@
+/*
+* copyright Eytoue (Lin Chenjun)
+* All rights reserved.
+*/
+
+#ifndef __CONFIG_H_
+#define __CONFIG_H_
+
+#ifndef BITS
+    #define BITS 32
+#endif /* BITS */
+
+#endif /* __CONFIG_H_ */
