c6d8c9a53dd28a178586b9ba4c4a39d904d7fd5c
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-17 11:04:11 -0600

    Update string.c

--- src/lib/string.c
+++ src/lib/string.c
@@ -1,8 +1,23 @@
 /* string.c */
 
+/* memset()
+* 功能:将dst__开始的size字节置为value
+* dst__ :要设置的地址
+* value :设置成的值
+* size  :要设置的大小(字节)
+*/
 void memset(void* dst__,uint8_t value,uint32_t size);
-
-
+{
+    ASSRET(dst != NULL)
+    uint8_t* dst = (uint8_t*)dst__;
+    int i;
+    for(i = 0;i < size;i++)
+    {
+        (*dst) = value;
+        dst++;
+    }
+    return;
+}
 void memcpy(void* dst__,const void* src__,uint32_t size);
 int32_t memcmp(const void* str_a__,void* str_b__,uint32_t size);
 
