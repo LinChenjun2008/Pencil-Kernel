19416bff79f1db49e1e8e7e6150cfde596901872
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-17 06:03:38 -0600

    Update imgcopy.cpp

--- tools/imgcopy/imgcopy.cpp
+++ tools/imgcopy/imgcopy.cpp
@@ -5,14 +5,11 @@
 
 #include "imgcopy.h"
 
-namespace global
-{
-    std::fstream vhd;
-}
+std::fstream vhd;
 
 int main(int argc,char* argv[])
 {
-    openvhd(argc,argv)
+    openvhd(argc,argv);
     return 0;
 }
 
@@ -30,13 +27,13 @@ void openvhd(int argc,char* argv[])
     if(i >= argc)
     {
         std::cerr << argv[0] << ':' << "not have  vhd file" << std::endl;
-        exit(-2);
+        exit();
     }
     global::vhd.open(argv[i],std::ios_base::in | std::ios_base::out | std::ios_base::binary);
     if(!(global::vhd.is_open()))
     {
         std::cerr << argv[0] << ':' << "can't open vhd file" << std::endl;
-        exit(-2);
+        exit();
     }
     std::cout << "open vhd file:" << argv[i] << std::endl;
 }
