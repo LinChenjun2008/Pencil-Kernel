a037560e90e2e72caf26f4599a1346cdec0e465c
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-17 22:39:28 -0600

    Create debug.h

---
+++ src/include/debug.h
@@ -0,0 +1,20 @@
+#ifndef __DEBUG_H_
+#define __DEBUG_H_
+
+void panic_spin(const char* file,int line,conat char* func,const char* condition);
+
+#ifndef NDEBUG
+    #define PANIC(...) panic_spin(__FILE__,__LINE__,__func__,_VA_ARGS__)
+    #define ASSRET(CONDITION)          \
+                if(CONDITION)          \
+                {                      \
+                                       \
+                }                      \
+                else                   \
+                {                      \
+                    PANIC(#CONDITION); \
+                }
+#else
+    #define ASSRET(CONDITION) ((void)0)
+#endif /* NDEBUG */
+#endif /* __DEBUG_H_ */
