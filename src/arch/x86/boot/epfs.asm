8c9cede863f56eb5813c22a50ba1e88e783f771f
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-19 07:26:22 -0600

    Update epfs.asm

--- src/arch/x86/boot/epfs.asm
+++ src/arch/x86/boot/epfs.asm
@@ -1,18 +1,15 @@
 jmp short start              ; 2B
 nop                          ; 3B
-Magic          db "EPFS01  " ; 8B
-DiskSize       dw 
-BytesPerSector dw 512
-
-
-read_super_block:
-    mov ax,0x00
-    mov es,ax
-    mov bx,0x8000;super_block的缓冲区
-    mov ax,0x2
-    mov cx,1
-    call read_sector
-    mov RootDirStart,[bx:(4*7)]
+Magic           db "EPFS01  " ; 8B
+DiskTotSec      dw 
+BytesPerSector  dw 512
+BitmapStartSec  dw
+BitmapSectors   dw             ;  4B bitmap占用的扇区数
+DataSecStart    dw             ;  4B 数据区起始扇区
+DataSectors     dw             ;  4B 数据区大小
+RootDirStartSec dw             ;  4B 根目录起始扇区
+RootDirSectors  dw             ;  4B 根目录占用扇区数
+                               ;465B 引导程序
+                               ;  2B 0x55,0xaa
+start:
     jmp $
-
-RootDirStart dw 0x0000
