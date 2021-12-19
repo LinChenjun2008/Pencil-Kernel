e668bcbfed058b44d53475a5bfc5eb8d7c184ee1
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-18 23:36:05 -0600

    Update imgcopy.h

--- tools/imgcopy/imgcopy.h
+++ tools/imgcopy/imgcopy.h
@@ -3,21 +3,6 @@
 
 #include "stdint.h"
 
-#ifndef _MAX_NAME_LEN_
-    #define MAX_NAME_LEN 15
-#endif
-
-struct file_desc              //64B
-{
-    char name[MAX_NAME_LEN];  //15B
-    uint8_t f_type;           // 1B
-    uint32_t f_size;          // 4B
-    uint32_t sectors[11];     //44B
-};
-
-struct index_block
-{
-    uint32_t sectors[128];    //4*128=512B
-};
+void vhd_format();
 
 #endif /* __IMGCOPY_H_ */
