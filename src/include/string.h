48817b65e62549705314cdefb33aa11123a5dc7e
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-19 05:56:53 -0600

    Update string.h

--- src/include/string.h
+++ src/include/string.h
@@ -3,7 +3,7 @@
 
 void memset(void* dst__,uint8_t value,uint32_t size);
 void memcpy(void* dst__,const void* src__,uint32_t size);
-int32_t memcmp(const void* addr_a__,void* addr_b__,uint32_t size);
+int32_t memcmp(const void* p1__,void* p2__,uint32_t size);
 
 char* strcpy(char* dst__,const char* src__);
 uint32_t strlen(const char* str);
