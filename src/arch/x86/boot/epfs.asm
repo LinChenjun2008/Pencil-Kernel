ba5b116168cf0e480d0285f6494a9e556f373687
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-19 07:30:48 -0600

    Update epfs.asm

--- src/arch/x86/boot/epfs.asm
+++ src/arch/x86/boot/epfs.asm
@@ -1,9 +1,9 @@
-jmp short start              ; 2B
-nop                          ; 3B
-Magic           db "EPFS01  " ; 8B
-DiskTotSec      dw 
-BytesPerSector  dw 512
-BitmapStartSec  dw
+jmp short start                ;  2B 跳转指令
+nop                            ;  3B 空指令
+Magic           db "EPFS01  "  ;  8B epfs文件系统名称
+TotSec          dw 2880        ;  4B 总扇区数
+BytesPerSector  dw 512         ;  4B 每扇区字节数
+BitmapStartSec  dw             ;  4B bitmap的起始扇区数
 BitmapSectors   dw             ;  4B bitmap占用的扇区数
 DataSecStart    dw             ;  4B 数据区起始扇区
 DataSectors     dw             ;  4B 数据区大小
