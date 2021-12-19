1dc327de804edeec7f68759f037db7d90656ae5d
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-19 04:36:58 -0600

    Update string.c

--- src/lib/string.c
+++ src/lib/string.c
@@ -43,29 +43,29 @@ void memcpy(void* dst__,const void* src__,uint32_t size)
 }
 
 /* memcmp
-* 功能:比较addr_a__和addr_b__处的size字节数据是否相等
-* addr_a__ :地址a
-* addr_b__ :地址b
+* 功能:比较p1__和p2__处的size字节数据是否相等
+* p1__ :地址1
+* p2__ :地址2
 * size     :要比较的字节数
 * 返回值:
-*   < 0 :addr_a__处 < addr_b__处
-*   > 0 :addr_a__处 > addr_b__处
-*   = 0 :addr_a__处 = addr_b__处
+*   < 0 :p1__处 < p2__处
+*   > 0 :p1__处 > p2__处
+*   = 0 :p1__处 = p2__处
 */
-int32_t memcmp(const void* addr_a__,void* addr_b__,uint32_t size)
+int32_t memcmp(const void* p1__,void* p2__,uint32_t size)
 {
-    ASSRET(addr_a__ != NULL & addr_b__ != NULL)
-    uint8_t* addr_a = (uint8_t*)addr_a__;
-    uint8_t* addr_b = (uint8_t*)addr_b__;
+    ASSRET(p1__ != NULL & p2__ != NULL)
+    uint8_t* p1 = (uint8_t*)addr_a__;
+    uint8_t* p2 = (uint8_t*)addr_b__;
     int i;
     for(i = 0;i < size;i++)
     {
-        if(*addr_a != *addr_b)
+        if(*p1 != *p2)
         {
             break;
         }
     }
-    return (int32_t)(*addr_a - *addr_b);
+    return (*p1 < *p2 ? -1 : *p1 > *p2);
 }
 
 char* strcpy(char* dst__,const char* src__);
