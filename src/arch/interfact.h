211286743b7a7ccc2cbb07d69421cccd970fb43d
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-21 20:59:42 +0800

    Update interfact.h

--- src/arch/interfact.h
+++ src/arch/interfact.h
@@ -7,10 +7,10 @@ struct x86
     uint32_t (*io_in8)(uint32_t port);
 };
 
-union arch /* 只能同时在一个平台,所以用union共用体 */
+union ARCH /* 只能同时在一个平台,所以用union共用体 */
 {
     struct x86 x86;
-};
+}arch;
 /* 调用与平台相关的函数:
 
 arch.平台.函数名(参数);
