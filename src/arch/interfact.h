cea1b9bf8de8df31644068f1ee00c8c37e5e9e00
Lin Chenjun <83539363+Eytoue@users.noreply.github.com>
2021-12-21 20:55:47 +0800

    Update interfact.h

--- src/arch/interfact.h
+++ src/arch/interfact.h
@@ -2,15 +2,6 @@
 #ifndef __INTERFACE_H_
 #define __INTERFACE_H_
 
-#define __FORMATS_ 1 /* 一个平台 */
-
-enum
-{
-    X86;
-    X64;
-    ARM;
-};
-
 struct x86
 {
     uint32_t (*io_in8)(uint32_t port);
@@ -20,5 +11,10 @@ union arch /* 只能同时在一个平台,所以用union共用体 */
 {
     struct x86 x86;
 };
+/* 调用与平台相关的函数:
+
+arch.平台.函数名(参数);
+
+*/
 
 #endif /* __INTERFACE_H_ */
