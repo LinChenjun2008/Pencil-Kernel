b42f087cded2ec9b9654507c14d9bbb3c9815eb0
Simon Lin <zpojia@hotmail.com>
2021-12-24 20:35:24 +0800

    Update list.h

--- src/include/list.h
+++ src/include/list.h
@@ -16,4 +16,17 @@ struct list
     struct list_elem list tail;
 };
 
+//初始化
+void list_init(struct list* L);
+//插入elem到in_before前面
+void list_in(struct lise_elem* elem,struct list_elem* in_before);
+//把elem添加到队首,类似于push
+void list_push(struct list* L,struct list_elem* elem);
+//添加elem到队尾
+void list_append(struct list* L,struct list_elem* elem);
+//将元素从链中退出
+struct list_elem* list_remove(struct list_elem* elem);
+//从队首弹出一个elem,类似于pop
+struct list_elem* list_pop(struct list* L);
+
 #endif /* __LIST_H_ */
