a77ec2a864325cd1e3a3fae8e1169382086c1b1a
Simon Lin <zpojia@hotmail.com>
2021-12-26 00:54:20 -0600

    Update list.h

--- src/include/list.h
+++ src/include/list.h
@@ -4,6 +4,8 @@
 #ifndef __LIST_H_
 #define __LIST_H_
 
+#include "global.h"
+
 /* offset
 * 求出member在struct_t类型的结构体中的地址偏移
 */
@@ -32,6 +34,6 @@ void list_push(struct list* L,struct list_elem* elem);
 void list_append(struct list* L,struct list_elem* elem);
 struct list_elem* list_remove(struct list_elem* elem);
 struct list_elem* list_pop(struct list* L);
-int list_find(struct list* L,struct list_elem* obj_elem);
+bool list_find(struct list* L,struct list_elem* obj_elem);
 
 #endif /* __LIST_H_ */
