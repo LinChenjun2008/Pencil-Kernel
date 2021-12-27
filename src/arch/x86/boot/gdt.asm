de3c4095b803fda7b44bbb2d2db47b1f3c858137
Simon Lin <zpojia@hotmail.com>
2021-12-27 19:33:54 +0800

    Update gdt.asm

--- src/arch/x86/boot/gdt.asm
+++ src/arch/x86/boot/gdt.asm
@@ -1,1 +1,8 @@
-GDT_BASE: dd 0x0000,0x0000 ;不可用描述符
+%macro DESC 3
+    dw (%2 & 0xffff)
+    dw (%1 & 0xffff)
+    db ((%1 >> 16) & 0xff)
+    db (%3 & 0xff)
+    db (((%2 >> 16)& 0x0f) | ((%3 >> 8)& 0xf0))
+    db ((%1 >> 24)& 0xff)
+%endmacro
