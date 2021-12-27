3fab3cfe26bbf5982cf84c5038894a4b5327f93e
Simon Lin <zpojia@hotmail.com>
2021-12-27 19:48:41 +0800

    Update gdt.asm

--- src/arch/x86/boot/gdt.asm
+++ src/arch/x86/boot/gdt.asm
@@ -2,12 +2,12 @@
 ;%1:base %2:limit %3:ar
 
 %macro DESC 3
-    dw (%2 & 0xffff)
-    dw (%1 & 0xffff)
-    db ((%1 >> 16) & 0xff)
-    db (%3 & 0xff)
-    db (((%2 >> 16)& 0x0f) | ((%3 >> 8)& 0xf0))
-    db ((%1 >> 24)& 0xff)
+    dw (%2 & 0xffff)      ;段界限15～0
+    dw (%1 & 0xffff)      ;段基址15～0
+    db ((%1 >> 16) & 0xff);段基址23～16
+    db (%3 & 0xff)        ;段属性 (P DPL S TYPE)
+    db (((%2 >> 16)& 0x0f) | ((%3 >> 8)& 0xf0)) ;段界限19～16(低位) 和段属性(G D/B L AVL)
+    db ((%1 >> 24)& 0xff)                       ;段基址31～24
 %endmacro
 
 GDT_PTR:
