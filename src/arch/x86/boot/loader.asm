5297785c5e37dc8b1559d237109cca474f779d14
Simon Lin <zpojia@hotmail.com>
2022-01-12 23:32:29 +0800

    Update loader.asm

--- src/arch/x86/boot/loader.asm
+++ src/arch/x86/boot/loader.asm
@@ -7,35 +7,41 @@ org 0x500
 %include "protect.inc"
 
 GDT_BASE: SEGMDESC 0,0,0
-SectionCode32:     SEGMDESC 0x00000000,0xfffff,AR_CODE32
-SectionData32:     SEGMDESC 0x00000000,0xfffff,AR_DATA32
-SectionVideo:      SEGMDESC 0x000b8000,0x00007,AR_DATA32
-SectionColorVideo: SEGMDESC 0x000a0000,0x0000e,AR_DATA32
+SectionCode32:     SEGMDESC 0x00000000,0xfffff,AR_CODE32 ;32位代码段
+SectionData32:     SEGMDESC 0x00000000,0xfffff,AR_DATA32 ;32位数据段
+SectionVideo:      SEGMDESC 0x000b8000,0x00007,AR_DATA32 ;文字显存
+SectionColorVideo: SEGMDESC 0x000a0000,0x0000e,AR_DATA32 ;彩色显存
 
 GDT_SIZE equ ($ - GDT_BASE)
 GDT_LIMIT equ GDT_SIZE - 1
 
 times 60 dq 0;预留60个描述符
 
-SelectorCode32     equ ((SectionCode32-GDT_BASE)/8 | TI_GDT | RPL0)
-SelectorData32     equ ((SectionData32-GDT_BASE)/8 | TI_GDT | RPL0)
-SelectorVideo      equ ((SectionVideo-GDT_BASE)/8 | TI_GDT | RPL0)
-SelectorColorVideo equ ((SectionColorVideo-GDT_BASE)/8 | TI_GDT | RPL0)
+;段选择子
+SelectorCode32     equ (((SectionCode32-GDT_BASE)/8) | TI_GDT | RPL0)
+SelectorData32     equ (((SectionData32-GDT_BASE)/8) | TI_GDT | RPL0)
+SelectorVideo      equ (((SectionVideo -GDT_BASE)/8) | TI_GDT | RPL0)
+SelectorColorVideo equ (((SectionColorVideo-GDT_BASE)/8) | TI_GDT | RPL0)
 
 gdt_ptr dw GDT_LIMIT
         dd GDT_BASE
 
 times (0x300 - ($ - $$)) db 0
-;&total_memory_bytes = 0x500+0x300 = 0x800
-total_memory_bytes dq 0 ;0x8
 
-ards_buf times 240 db 0 ;ards 0x
-ards_nr dw 0;0x4
-;0x300 + 0x40 + 0x100 = 0x440
+;&total_memory_bytes = 0x500+0x300 = 0x800
+total_memory_bytes dq 0 ;内存大小(单位:字节)
+
+ards_buf times 240 db 0
+ards_nr dw 0
+
 times (0x500-($ - $$)) db 0;对齐到文件起始0x500处
+
 ;$ =0x500 + 0x500 = 0xa00
+
+;loader从此处开始执行
 start:
-;获取内存布局
+
+; int 0x15 edx=0x534d4150:获取内存布局
     xor ebx,ebx       ;将ebx清零
     mov edx,0x534d4150;edx = "SMAP"
     mov di,ards_buf
