d403ba6883406175eb4c02678c4c9e4134d4e7bb
Simon Lin <zpojia@hotmail.com>
2022-01-17 21:26:50 +0800

    Update epfs.asm

--- src/arch/x86/boot/epfs.asm
+++ src/arch/x86/boot/epfs.asm
@@ -1,19 +1,6 @@
 ;epfs.asm
 ;Copyright 2021-2022 Lin Chenjun,All rights reserved.
 
-jmp short start                ;  2B 跳转指令
-nop                            ;  3B 空指令
-Magic           db "EPFS  01"  ;  8B epfs文件系统名称
-TotSec          dw 131072      ;  4B 总扇区数 (131072 * 512)B = 64MB
-BytesPerSector  dw 512         ;  4B 每扇区字节数
-BitmapStartSec  dw 1           ;  4B bitmap的起始扇区数(LBA)
-BitmapSectors   dw             ;  4B bitmap占用的扇区数
-DataStartSec    dw             ;  4B 数据区起始扇区(LBA)
-DataSectors     dw             ;  4B 数据区大小
-RootDirStartSec dw             ;  4B 根目录起始扇区(LBA)
-RootDirSectors  dw             ;  4B 根目录占用扇区数
-                               ;465B 引导程序
-                               ;  2B 0x55,0xaa
 FileName: db "loader.bin     " ;文件名15B
 RootDirSizeForLoop dw RootDirSectors
 SectorNo dd 0 ;下一次要读取的扇区号
