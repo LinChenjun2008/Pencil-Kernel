c319a5e152a2cf313a4b2f2d691e6bba72d17852
Simon Lin <zpojia@hotmail.com>
2022-01-17 21:26:32 +0800

    Update boot.asm

--- src/arch/x86/boot/boot.asm
+++ src/arch/x86/boot/boot.asm
@@ -2,19 +2,9 @@
 
 org 0x7c00
 [bits 16]
-jmp short start                ;  2B 跳转指令
-nop                            ;  3B 空指令
-Magic           db "EPFS  01"  ;  8B epfs文件系统名称
-TotSec          dw 131072      ;  4B 总扇区数 (131072 * 512)B = 64MB
-BytesPerSector  dw 512         ;  4B 每扇区字节数
-BitmapStartSec  dw 1           ;  4B bitmap的起始扇区数(LBA)
-BitmapSectors   dw (((((64*1024*1024)/512)/8)/8)/512) ;  4B bitmap占用的扇区数.对于这里的64M软盘来说应该是4
-DataStartSec    dw             ;  4B 数据区起始扇区(LBA)
-DataSectors     dw             ;  4B 数据区大小
-RootDirStartSec dw             ;  4B 根目录起始扇区(LBA)
-RootDirSectors  dw             ;  4B 根目录占用扇区数
-                               ;465B 引导程序
-                               ;  2B 0x55,0xaa
+%include "epfs.inc"
+
+
 
 ;%include "epfs.asm"
     jmp $
