3e48ce44f1c873f1c21a8e8c77b0690f5f0d9f22
Simon Lin <zpojia@hotmail.com>
2022-01-20 15:03:18 +0800

    Update debug.c

--- src/arch/x86/kernel/debug.c
+++ src/arch/x86/kernel/debug.c
@@ -1,7 +1,7 @@
 /* copyright Lin Chenjun
 * All rights reserved
 */
-void panic_spin(const char* file,int line,conat char* func,const char* condition)
+void panic_spin(const char* file,int line,const char* func,const char* condition)
 {
     while(1){;}
     return;
