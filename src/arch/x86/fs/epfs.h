1d49ea25ce63d01132eb7fabc05d7567f0a7afd2
Simon Lin <zpojia@hotmail.com>
2022-01-22 06:02:52 -0600

    Update epfs.h

--- src/arch/x86/fs/epfs.h
+++ src/arch/x86/fs/epfs.h
@@ -39,19 +39,26 @@
 
 struct index_block
 {
-    uint8_t Magic[8];                          //  8B 文件系统名称
-    uint32_t TotSec;                           //  4B 总扇区数
-    uint32_t BytesPerSector;                   //  4B 每扇区大小(byte)
-    uint32_t InodeStartSec;
-    uint32_t InodeSectors;
-    uint32_t BitmapStartSec;                   //  4B bitmap的起始扇区
-    uint32_t BitmapSectors;                    //  4B bitmap占用的扇区数
-    uint32_t DataStartSec;                     //  4B 数据区起始扇区
-    uint32_t DataSectors;                      //  4B 数据区大小
-    uint32_t RootDirStartSec;                  //  4B 根目录起始扇区
-    uint32_t RootDirSectors;                   //  4B 根目录占用扇区数
-    uint8_t ipl[510 - ((4 * 8)+(5 + 8))];      //465B 引导程序
-    uint8_t end[2];                            //  2B 结束标志0x55,0xaa
+    /* 基本信息 */
+    uint8_t Magic[8];             /* 用于识别文件系统 */
+    uint32_t StartSec;            /* 分区起始扇区 */
+    uint32_t TotSec;              /* 用于记录分区总扇区数 */
+    uint32_t BytesPerSector;      /* 每扇区字节数(应该都是512吧?) */
+
+    /* inode信息 */
+    uint32_t InodeCnt;            /* inode数量 */
+    uint32_t InodeStartSec        /* inode起始扇区 */
+    uint32_t InodeSectors         /* inode占用扇区数 */
+    uint32_t RootDirInodeNo       /* 根目录的inode编号 */
+
+    /* bitmap */
+    uint32_t BlockBitmapStartSec; /* 用于记录块使用信息的bitmap */
+    uint32_t BlockBitmapSectors;  /* 块bitmap占用的扇区数 */
+    uint32_t InodeBitmapStartSec; /* 用于记录inode使用情况的bitmap */
+    uint32_t InodeBitmapSectors;  /* inode bitmap占用的扇区数 */
+
+    /* 数据区 */
+    uint32_t DataStartSec;        /* 数据区起始扇区 */
 }PACKED;//512B
 
 struct file_desc
