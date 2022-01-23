806ac8e13223209a7288848d0b898c7c8cad8b8d
Simon Lin <zpojia@hotmail.com>
2022-01-23 00:26:32 -0600

    Update start.asm

--- src/arch/x86/kernel/start.asm
+++ src/arch/x86/kernel/start.asm
@@ -1,6 +1,8 @@
 ;Pencil Kernel start.asm
-;copyright 2022 LinChenjun, All rights reserved
+;copyright 2022 LinChenjun, All rights reserved.
+extern _main
 [bits 32]
 section .text
 _start:
+    jmp _main
     jmp $
