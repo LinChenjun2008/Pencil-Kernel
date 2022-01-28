729d516dd3b59759f226eff558056df71400af07
Simon Lin <zpojia@hotmail.com>
2022-01-28 14:11:18 +0800

    Update config.h

--- src/arch/x86/include/config.h
+++ src/arch/x86/include/config.h
@@ -5,7 +5,12 @@
 #ifndef __CONFIG_H_
 #define __CONFIG_H_
 
-/* #define __UI_GRAPHIC__ */
-#define __UI_TEXT__
+/* ui界面,文本还是图形
+* 只能选一个
+* 同时要在boot.inc也做同样的选择
+*/
+
+/* #define __UI_GRAPHIC__ /* 图形界面(暂不支持) */ */
+#define __UI_TEXT__ /* 文本界面 */
 
 #endif /* __CONFIG_H_ */
