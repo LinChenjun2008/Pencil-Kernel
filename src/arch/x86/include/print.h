704ea8fc1a1052a9c3d78045e496838e888690ee
LineMK <zpojia@hotmail.com>
2022-01-29 07:06:13 -0600

    Update print.h

--- src/arch/x86/include/print.h
+++ src/arch/x86/include/print.h
@@ -1,12 +1,17 @@
 #ifndef __PRINT_H__
 #define __PRINT_H__
 
-#define VRAM (0xc0000000 + 0xb8000)
+#include "config.h"
+
+#ifdef __UI_TEXT__
+    #define VRAM (0xc0000000 + 0xb8000) /* 显存地址 */
+    #define ROW 25 /* 默认共25行 */
+    #define COL 80 /* 默认共80列 */
+#endif /* __UI_TEXT__ */
 
 // #include "stdint.h"
 
-// #define ROW 25 /* 共25行 */
-// #define COL 80 /* 共80列 */
+
 
 // int put_string_xy(char* str,int row,int col,uint8_t fgc,uint8_t bgc);
 // int put_string(char* str,uint8_t fgc,uint8_t bgc);
