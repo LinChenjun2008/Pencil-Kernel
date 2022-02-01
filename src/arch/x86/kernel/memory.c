21a4996ce3c70c257a672e1463f45c302b1f014b
Linevtnpu <zpojia@hotmail.com>
2022-02-01 07:16:36 -0600

    Update memory.c

--- src/arch/x86/kernel/memory.c
+++ src/arch/x86/kernel/memory.c
@@ -5,11 +5,11 @@
 
 void init_mem_pool(uint32_t total_mem)
 {
-    uint32_t page_table_size = PG_SIZE * 256; /* 页表大小:1页页目录 + 第0和第768页目录指向同一个页表 + 第769~第1022页目录指向第245个页表,共256个页框,共计2MB */
-    uint32_t bitmap_used_mem = (((4*1024)/128)*0x1000); /* bitmap用掉的内存 */
-    uint32_t used_mem = page_table_size + 0x100000 + bitmap_used_mem; /* 
-    uint32_t free_mem = total_mem - used_mem;
-    uint32_t total_free_mem = free_mem / PG_SIZE;
+    uint32_t page_table_size = PG_SIZE * 256;                         /* 页表大小:1页页目录 + 第0和第768页目录指向同一个页表 + 第769~第1022页目录指向第245个页表,共256个页框,共计2MB */
+    uint32_t bitmap_used_mem = (((4*1024)/128)*0x1000);               /* bitmap用掉的内存 */
+    uint32_t used_mem = page_table_size + 0x100000 + bitmap_used_mem; /* 已使用内存 */
+    uint32_t free_mem = total_mem - used_mem;                         /* 空闲内存 */
+    uint32_t total_free_mem = free_mem / PG_SIZE;                     /* 全部的可用页数量 */
 
     
 }
