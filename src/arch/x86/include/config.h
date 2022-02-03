37a6ac9c2657134f71a802fef14728a33ae5053f
LinChenjun2008 <zpojia@hotmail.com>
2022-02-02 23:33:21 -0600

    Update config.h

--- src/arch/x86/include/config.h
+++ src/arch/x86/include/config.h
@@ -2,15 +2,13 @@
 * 系统内核配置文件
 */
 
-#ifndef __CONFIG_H_
-#define __CONFIG_H_
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
 
-/* ui界面,文本还是图形
-* 只能选一个
-* 同时要在boot.inc也做同样的选择
-*/
+enum Display
+{
+    _TEXT = 0,
+    _GRAPHIC = 1,
+};
 
-/* #define __UI_GRAPHIC__ /* 图形界面(暂不支持) */
-#define __UI_TEXT__ /* 文本界面 */
-
-#endif /* __CONFIG_H_ */
+#endif /* __CONFIG_H__ */
