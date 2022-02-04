5ec5a21833146a59435fe8840e0123e54f831d91
LinChenjun <zpojia@hotmail.com>
2022-02-03 20:56:17 -0600

    Update memory.c

--- src/arch/x86/kernel/memory.c
+++ src/arch/x86/kernel/memory.c
@@ -5,13 +5,15 @@
 
 void init_mem_pool(uint32_t total_mem)
 {
-    uint32_t page_table_size = PG_SIZE * 256;                         /* 页表大小:1页页目录 + 第0和第768页目录指向同一个页表 + 第769~第1022页目录指向第245个页表,共256个页框,共计2MB */
+    uint32_t page_table_size = PG_SIZE * 256; /* 页表大小:1页页目录 + 第0和第768页目录指向同一个页表 + 第769~第1022页目录指向第245个页表,共256个页框,共计2MB */
+
     uint32_t bitmap_used_mem = (((4*1024)/128)*0x1000);               /* bitmap用掉的内存 */
     uint32_t used_mem = page_table_size + 0x100000 + bitmap_used_mem; /* 已使用内存 */
     uint32_t free_mem = total_mem - used_mem;                         /* 空闲内存 */
-    uint32_t total_free_pages = free_mem / PG_SIZE;                   /* 全部的可用页数量 */
-    uint32_t kernel_free_pages = total_free_pages / 2;                /* 内核可用页 */
-    uint32_t user_free_pages = total_free_pages - kernel_free_pages;  /* 用户可用页 */
+
+    uint32_t total_free_pages = free_mem / PG_SIZE;                  /* 全部的可用页数量 */
+    uint32_t kernel_free_pages = total_free_pages / 2;               /* 内核可用页 */
+    uint32_t user_free_pages = total_free_pages - kernel_free_pages;./* 用户可用页 */
     
     
 }
