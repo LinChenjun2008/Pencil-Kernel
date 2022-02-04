bcc97e9c2557c674020f0fc379fb15a508e24e07
LinChenjun <zpojia@hotmail.com>
2022-02-03 23:36:15 -0600

    Update memory.h

--- src/arch/x86/include/memory.h
+++ src/arch/x86/include/memory.h
@@ -10,14 +10,14 @@
 struct virtual_addr
 {
     struct bitmap vaddr_bitmap; /* 虚拟地址用到的内存bitmap */
-    uint32_t vaddr_start;       /* 虚拟地址起始处 */
+    void* vaddr_start;          /* 虚拟地址起始处.因为是地址,所以用指针 */
 };
 
 struct pool
 {
     struct bitmap pool_bitmap;  /* 用于管理物理内存的bitmao */
-    uint32_t phy_address_start; /* 所管理的物理内存起始地址 */
-    uint32_t pool_size;         /* 内存池字节容量 */
+    void* phy_address_start;    /* 所管理的物理内存起始地址,因为是地址,所以用指针 */
+    uint64_t pool_size;         /* 内存池字节容量 */
 };
 
 extern struct pool kernel_pool;
