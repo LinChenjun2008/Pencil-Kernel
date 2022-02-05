e30281e96b08206d8f57f2587ec3a6c386c35f68
LinChenjun <zpojia@hotmail.com>
2022-02-05 07:08:22 -0600

    Update mbr.asm

--- src/arch/x86/boot/mbr.asm
+++ src/arch/x86/boot/mbr.asm
@@ -4,5 +4,16 @@
 org 0x7c00
 
 times 446 - ($ - $$) db 0;硬盘分区表
-times 510 - ($ - $$) db 0;其实不写这个也没关系,这段空间会被硬盘分区表填充
+part1:
+    db 0x80 ;活动分区标记
+    db 0
+    db 0
+    db 0
+    db 0x95 ;分区类型:0x95,是Pencil-Kernel使用的EPFS
+    db 0
+    db 0
+    db 0
+    dd 0x3f
+    dd ((64*1024*1024)/512)
+times 510 - ($ - $$) db 0;这段空间会被硬盘分区表填充
 db 0x55,0xaa
