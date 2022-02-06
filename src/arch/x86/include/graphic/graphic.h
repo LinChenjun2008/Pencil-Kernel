77dc8cc05b80fca69b725f959b849f4bf63768df
LinChenjun <zpojia@hotmail.com>
2022-02-06 05:52:26 -0600

    Update graphic.h

--- src/arch/x86/include/graphic/graphic.h
+++ src/arch/x86/include/graphic/graphic.h
@@ -1,6 +1,6 @@
 #ifndef __GRAPHIC_H__
 #define __GRAPHIC_H__
 
-#define rgba(R,G,B,A) ((uint32_t)(0x00000000 |( A << 24 | R << 16 | G << 8 | B << 0 )))
+#define rgba(R, G, B, A) ((uint32_t)(0x00000000 |( A << 24 | R << 16 | G << 8 | B << 0 )))
 
 #endif /* __GRAPHIC_H__ */
