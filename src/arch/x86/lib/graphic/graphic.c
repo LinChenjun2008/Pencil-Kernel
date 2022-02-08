77ad6351e6d3792a0b55a76138868ce2ec022e96
LinChenjun <zpojia@hotmail.com>
2022-02-08 07:36:15 -0600

    Update graphic.c

--- src/arch/x86/lib/graphic/graphic.c
+++ src/arch/x86/lib/graphic/graphic.c
@@ -1,3 +1,17 @@
 #include "graphic.h"
 
-void RectangleFill(void* _vram,int xsize,uint32_t color,int x0,int y0,int x1,int y1)
+#include "stdint.h"
+
+void RectangleFill(uint32_t* vram,int xsize,uint32_t color,int x0,int y0,int x1,int y1)
+{
+    int x;
+    int y;
+    for(y = y0;y < y1;y++)
+    {
+        for(x = x0;x < x1;x++)
+        {
+            *(vram + (y * xsize + x)) = color;
+        }
+    }
+    return;
+}
