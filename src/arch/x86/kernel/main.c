e92ed082eb92bb591ff24d6fb91d24746efa62b2
LinChenjun <zpojia@hotmail.com>
2022-02-08 05:40:41 -0600

    Update main.c

--- src/arch/x86/kernel/main.c
+++ src/arch/x86/kernel/main.c
@@ -11,13 +11,14 @@ int main()
     put_str("Pencil-Kernel (PKn) version 0.0.0 test\n");
     init_all();
     put_str("init down!\n");
+    /* 测试显存的映射 */
     int* addr = (int*)0xfe000000;
     for(i = 0;i < (ScrnX)*(ScrnY);i++)
     {
         *((char*)addr + 0) = (char)0x84; /* B */
         *((char*)addr + 1) = (char)0x84; /* G */
         *((char*)addr + 2) = (char)0x00; /* R */
-        *((char*)addr + 3) = (char)0x00;
+        *((char*)addr + 3) = (char)0x00; /* reserved */
         addr+=1;
     }
     //intr_enable(); /* 开中断 */
