79600b8480800adb47f1cdd73186d8e40a9b24ea
LinChenjun <zpojia@hotmail.com>
2022-02-08 07:44:10 -0600

    Update graphic.h

--- src/arch/x86/include/graphic/graphic.h
+++ src/arch/x86/include/graphic/graphic.h
@@ -4,4 +4,13 @@
 #define rgb(R,G,B) ((uint32_t)(0x00000000 |( R << 16 | G << 8 | B << 0 )))
 #define rgba(R,G,B,A) ((uint32_t)(0x00000000 |( A << 24 | R << 16 | G << 8 | B << 0 )))
 
+struct Rectangle
+{
+    uint32_t* vram;     /* 显示缓冲区地址 */
+    uint32_t xsize;     /* x长度 */
+    uint32_t ysize;     /* y长度 */
+    uint32_t XPostiton; /* 左上x坐标 */
+    uint32_t YPostiton; /* 左上y坐标 */
+};
+
 #endif /* __GRAPHIC_H__ */
