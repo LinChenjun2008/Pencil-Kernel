10e683215f99530469b9fe4d30645d82090d932d
LinChenjun2008 <zpojia@hotmail.com>
2022-02-09 13:54:05 +0800

    Update

--- src/arch/x86/boot/mbr.asm
+++ src/arch/x86/boot/mbr.asm
@@ -7,7 +7,7 @@
 ; 3. 寻找可引导分区,把分区前512字节加载到0x7c00地址处
 ; 4. 跳转到0x7c00,mbr结束
 
-;org 0x7c00
+org 0x7c00
 [bits 16]
 Start:
     ;初始化寄存器 (Initialize registers)
@@ -26,13 +26,28 @@ Start:
     mov di,0
     mov si,0
     cld      ;清零方向标志位df,df默认是0,所以不写也没问题,但还是写上放心
-    rep movw ;从ds:si复制一个字(word)到es:di,复制次数:cx的值
+    rep movsw ;从ds:si复制一个字(word)到es:di,复制次数:cx的值
              ;每次复制后,si和di的值会对应数据大小增加(df位为1时会减小)
+        mov bp,Msg
+        mov cx,3      ;3个字符 (chars:3)
+        mov ax,0x1301
+        mov bx,0x0007 ;第0页,黑底白字 (Page:0,Background color:black)
+        mov dx,0x0000 ;行,列 (row and col)
+        int 0x10
+
     jmp 0x7f0:next
 ;org 0x7f00
     next: ;这里就是复制到0x7f00后的mbr了
         mov ax,0x7f0
         mov ds,ax ;因为没有用org,所以要向ds载入段值
+
+        mov bp,Msg
+        mov cx,3      ;3个字符 (chars:3)
+        mov ax,0x1301
+        mov bx,0x0007 ;第0页,黑底白字 (Page:0,Background color:black)
+        mov dx,0x0000 ;行,列 (row and col)
+        int 0x10
+
         ;接下来要寻找活动分区
         ;只管第一分区
         .part1
@@ -44,6 +59,10 @@ Start:
         mov eax,[part1 + 8] ;分区起始lba扇区号
         jmp .load_boot
         .part2;暂时不管第二分区
+            mov ax,0xb800
+            mov gs,ax
+        mov byte [gs: 0x00],'E'
+        mov byte [gs: 0x01],0x04
             jmp $
         .load_boot
             mov cx,1 ;1扇区
@@ -128,7 +147,7 @@ DiskAddressPacket:
     dq 0    ;+ 8 扇区起始号(LBA模式)(Start Sector number(LBA mode))
     dq 0    ;+10 64位缓冲区地址拓展 (未始用)(64-bit buffer address extension(unusing))
 
-
+Msg db "MBR"
 times 446 - ($ - $$) db 0
 ;硬盘分区表
 part1:
