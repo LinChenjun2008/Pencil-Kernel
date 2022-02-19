13f3f38adc9bb2d18df01a52b8104d87f7fa7c5b
LinChenjun <zpojia@hotmail.com>
2022-02-19 02:35:46 -0600

    Update init.h

--- src/arch/x86/include/init.h
+++ src/arch/x86/include/init.h
@@ -1,6 +1,15 @@
 #ifndef __INIT_H__
 #define __INTI_H__
 
+#if (defined(__cplusplus))
+    extern "C"
+    {
+#endif
+
 void init_all();
+
+#if (defined(__cplusplus))
+    }
+#endif
 
 #endif /* __INIT_H__ */
