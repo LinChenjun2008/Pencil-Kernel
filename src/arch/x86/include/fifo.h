20d0a096e1cf2bb59b42ba1a5f28d14b1cf8f462
LinChenjun <zpojia@hotmail.com>
2022-03-15 08:36:33 -0500

    Create fifo.h

---
+++ src/arch/x86/include/fifo.h
@@ -0,0 +1,21 @@
+#ifndef __FIFO_H__
+#define __FIFO_H__
+
+#include "stdint.h"
+
+struct FIFO
+{
+    union
+    {
+        uint8_t* buf8;
+        uint16_t* buf16;
+        uint32_t* buf32;
+    }buf;
+    int type;
+    int size;
+    int nr;
+    int nw;
+    int flage;
+};
+
+#endif /* __FIFO_H__ */
