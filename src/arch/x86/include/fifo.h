9fd07e3a67b98fd87b3c05342311228e12e17ded
LinChenjun <zpojia@hotmail.com>
2022-03-17 08:30:09 -0500

    Update fifo.h

--- src/arch/x86/include/fifo.h
+++ src/arch/x86/include/fifo.h
@@ -12,11 +12,11 @@ struct FIFO
         uint32_t* buf32;
         uint64_t* buf64;
     };
-    int type;
-    int size;
+    int type; /* 类型(8,16,32或64) */
+    int size; /* 大小(最大元素数) */
     int nr;
     int nw;
-    int flage;
+    int flage; /* 状态标记 */
 };
 
 #endif /* __FIFO_H__ */
