e79d431f0e0f8aefac4059d66a8c7ba54a075f30
LinChenjun <zpojia@hotmail.com>
2022-03-17 06:40:15 -0500

    Update fifo.c

--- src/arch/x86/lib/fifo.c
+++ src/arch/x86/lib/fifo.c
@@ -10,3 +10,19 @@ void init_fifo(struct FIFO* fifo,void* buf,int type,int size)
     fifo->nr = 0;
     fifo->nw = 0;
 }
+
+int fifo_put(struct FIFO* fifo,void* data)
+{
+    if(fifo->free == 0)
+    {
+        fifo->flage = 0x01;
+        return -1
+    }
+    switch(fifo->type)
+    {
+        case 8:
+            fifo->buf8[fifo->nw] = *((uint8_t*)data);
+            break;
+    }
+    return 0;
+}
